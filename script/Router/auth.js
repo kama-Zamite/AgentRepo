c8a94025c90697e9f3e2f008a438aa90b8df0d8a
Pedro Kama Zamite <pzamite@c3r5s8.42luanda.com>
2026-02-11 10:49:15 +0100

    deplay proj

---
+++ script/Router/auth.js
@@ -0,0 +1,26 @@
+document.querySelector("form").addEventListener("submit", function(e) {
+    e.preventDefault();
+
+    const username = document.getElementById("username").value;
+    const password = document.getElementById("password").value;
+    const Error = document.getElementById("Error");
+    
+    const users = [
+        {username: "admin", password: "admin123", refe: "admin"},
+        {username: "aluno", password: "aluno123", refe: "aluno"},
+    ];
+
+    const user = users.find(u => { 
+        return u.username === username && u.password === password; 
+    });
+    if (!user)
+    {
+        Error.textContent = "Invalid username or password.";
+        return;
+    }
+    
+    if (user.refe === "aluno")
+        window.location.href = "../page/layout/aluno.html";
+    else if (user.refe === "admin")
+        window.location.href = "../page/layout/admin.html";
+});
